c0aa7befbf5ffe6b628db12676f90aa52e292ded
sodastsai <sodas2002@gmail.com>
2016-03-23 23:30:14 +0800

    Add empty project

---
+++ Airports/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Airports
+//
+//  Created by sodas on 3/23/16.
+//  Copyright © 2016 sodas. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
